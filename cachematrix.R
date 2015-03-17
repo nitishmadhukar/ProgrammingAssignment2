7e708950a07b0fe7499c641096287ba4e50dc5ae
Nitish Madhukar <nitishmadhukar@gmail.com>
2015-03-17 23:50:41 +0530

    Assignment complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program has two functions 1. makeCacheMatrix and 2. cacheSolve
+## 1. makeCacheMatrix accepts a Square matrix that is nonsingular and returns a list
+## 2. cacheSolve accepts the result of makeCacheMatrix and returns the inverse of matrix from the cache (if available)
 
-## Write a short comment describing this function
+## makeCacheMatrix receives a Nonsingular square matrix and returns a list containing
+## 1. get and set functions for the matrix (getmatrix and setmatrix)
+## 2. get and set functions for the inverse of the matrix (getinverse and setinverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getmatrix <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set = setmatrix, get = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve receives the result of makeCacheMatrix above and 
+## returns the inverse of the matrix accepted by makeCacheMatrix.
+## The inverse is calculated in this function if the inverse is not already calculated in makeCacheMatrix
+## The inverse is obtained from the cache if the inverse is already calculted in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse) ## Inverse of matrix available in the cache
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse ## Inverse of matrix calculted here        
 }
